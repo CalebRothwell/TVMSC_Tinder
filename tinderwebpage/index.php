d88479ba7d919e6c155b109eeea85fa4c4ae3870
Caleb Rothwell <cal.rothwell@gmail.com>
2015-03-13 08:33:36 -0600

    this weeks progress

---
+++ tinderwebpage/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+      <form action="login.php" method="post"
+
+      Username: <input type="text" name="username"> <p>
+      Password: <input type="password" name="password"> <p>
+                <input type="submit" value= "login">
+
+      </form>
+
+</html>
---
+++ tinderwebpage/login.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if ($username && $password)
+{
+  $connect = mysql_connect("localhost", "root", "") or die("Couldn't conect to the database!");
+  mysql_connect_db("login1") or die("Couldn't find database");
+
+  $query = mysql_query("SELECT * FROM users where username= '$username'");
+
+  $numsrows = mysql_num_rows($query);
+
+  if($numsrows!==0)
+  {
+    while($row = mysql_fetch_assoc($query))
+    {
+      $dbusername = $row['username'];
+      $dbpassowrd = $row['password'];
+    }
+    if($username==dbusername&&$password)==$dbpassword)
+    {
+      echo "you are logged in!";
+
+      @$_SESSION['username'] = $username;
+  }
+    else
+        echo "Your password is incorrect"
+ }
+    else
+        die("That user doesn't exist")
+}
+  else
+      die("Please enter a username and passoword!")
+
+?>
