7ac459556146a92061fb7062ae7758abb3bc3a5e
Caleb Rothwell <cal.rothwell@gmail.com>
2015-04-17 14:44:28 -0600

    this weeks commit

---
+++ tinderwebpage/profile.php
@@ -0,0 +1,20 @@
+<!Doctype html>
+<html lang="en-US">
+<html>
+<head>
+  <title> profile page </title>
+  <head/>
+    <p1> Upload a profile picture and add a short description about yourself </p1>
+
+      Select images: <input type="file" name="img" multiple>
+      <input type="submit"> // this is where the user can upload a profile picture
+    </form>
+      <textarea cols="50" rows="5">
+
+      </textarea // This is where the user can enter text.
+
+
+
+
+    </body>
+  </html>
